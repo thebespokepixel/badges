2e02491203ec31d6e7ad7482346307266f156fbf
Mark Griffiths <mark@thebespokepixel.com>
2016-10-14 14:37:49 +0100

    Squashed commit of the following:
    commit 7582fd9a07a52af95614f7e8a3131502535c927a
    Author: Mark Griffiths <mark@thebespokepixel.com>
    Date:   Fri Oct 14 14:35:47 2016 +0100
    
        Build out architecture

---
+++ src/lib/providers/codeclimate.js
@@ -0,0 +1,49 @@
+import _ from 'lodash'
+import node from 'unist-builder'
+
+// [code-climate-badge]: https://codeclimate.com/github/MarkGriffiths/badges/badges/gpa.svg
+// [coverage-badge]: https://codeclimate.com/github/MarkGriffiths/badges/badges/coverage.svg
+// [code-climate]: https://codeclimate.com/github/MarkGriffiths/badges
+// [coverage]: https://codeclimate.com/coverage/github/MarkGriffiths/badges
+
+export function cc(config, user) {
+	return node('paragraph', [
+		node('link', {
+			title: _.upperFirst(config.title),
+			url: `https://codeclimate.com/github/${
+				user.github.slug
+			}`
+		}, [
+			node('image', {
+				alt: _.upperFirst(config.title),
+				url: `https://codeclimate.com/github/${
+					user.github.slug
+				}/badges/gpa.svg?style=${
+					config.style
+				}`
+			})
+		]),
+		node('text', ' ')
+	])
+}
+
+export function ccCoverage(config, user) {
+	return node('paragraph', [
+		node('link', {
+			title: _.upperFirst(config.title),
+			url: `https://codeclimate.com/coverage/github/${
+				user.github.slug
+			}`
+		}, [
+			node('image', {
+				alt: _.upperFirst(config.title),
+				url: `https://codeclimate.com/github/${
+					user.github.slug
+				}/badges/coverage.svg?style=${
+					config.style
+				}`
+			})
+		]),
+		node('text', ' ')
+	])
+}
---
+++ src/lib/providers/rollup.js
@@ -0,0 +1,26 @@
+import _ from 'lodash'
+import node from 'unist-builder'
+import urlencode from 'urlencode'
+import {renderIcon} from '../utils'
+
+// [es-badge]: https://img.shields.io/badge/es2015-jsnext:main_✔-64CA39.svg?style=flat&logo=
+// [es]: https://github.com/rollup/rollup/wiki/jsnext:main
+
+export default function render(config, user) {
+	return node('paragraph', [
+		node('link', {
+			title: _.upperFirst(config.title),
+			url: `https://github.com/rollup/rollup/wiki/jsnext:main`
+		}, [
+			node('image', {
+				alt: _.upperFirst(config.title),
+				url: `https://img.shields.io/badge/es2015-${
+					urlencode('jsnext:main_✔')
+				}-64CA39.svg?style=${
+					config.style
+				}${config.icon && renderIcon('rollup')}`
+			})
+		]),
+		node('text', ' ')
+	])
+}
