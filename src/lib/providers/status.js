59de247a929716928618ce596075091a524eb490
Mark Griffiths <mark@thebespokepixel.com>
2016-10-16 17:37:51 +0100

    Add test

--- src/lib/providers/status.js
+++ src/lib/providers/status.js
@@ -9,9 +9,7 @@ import node from 'unist-builder'
 export default function render(config) {
 	const badgeNode = node('image', {
 		alt: _.upperFirst(config.title),
-		url: `http://img.shields.io/badge/${
-			config.title
-		}-${
+		url: `http://img.shields.io/badge/status-${
 			config.text
 		}-${
 			config.color
--- test/main.js
+++ test/main.js
@@ -1,8 +1,8 @@
 import test from 'ava'
-
 import badges from '..'
 
-test(`Module name is '${pkg.name}'.`, t => {
-	t.is(`${pkg.name}`, myMeta.name)
+test(`Simple status`, async t => {
+	const content = await badges('test-1')
+	t.is(content, '![Status](http://img.shields.io/badge/status-beta-blue.svg?style=flat) \n\n  \n\n')
 })
 
