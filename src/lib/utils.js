f8ebb70e773281f7426b6a9f421c8a3c6909b699
Mark Griffiths <mark@thebespokepixel.com>
2016-10-16 17:49:17 +0100

    Fix xo warnings

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,11 +2,20 @@ import {readFileSync} from 'fs'
 import urlencode from 'urlencode'
 import {gte} from 'semver'
 
-export function renderIcon(id) {
+export function renderIconSVG(id) {
 	const iconSource = readFileSync(`./icons/${id}.svg`)
 	const iconBuffer = gte(process.version, '6.0.0') ?
 		Buffer.from(iconSource) :
 		new Buffer(iconSource)
 
 	return `&logo=${urlencode(`data:image/svg+xml;base64,${iconBuffer.toString('base64')}`)}`
+}
+
+export function renderIconPNG(id) {
+	const iconSource = readFileSync(`./icons/${id}.png`)
+	const iconBuffer = gte(process.version, '6.0.0') ?
+		Buffer.from(iconSource) :
+		new Buffer(iconSource)
+
+	return `&logo=${urlencode(`data:image/png;base64,${iconBuffer.toString('base64')}`)}`
 }
