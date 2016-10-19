94cae16279dc216155fc1e14cfe9a88210695ad7
Mark Griffiths <mark@thebespokepixel.com>
2016-10-19 10:51:55 +0100

    Add AST export

--- test/main.js
+++ test/main.js
@@ -6,3 +6,30 @@ test(`Simple status`, async t => {
 	t.is(content, '![Status](http://img.shields.io/badge/status-beta-blue.svg?style=flat) \n\n  \n\n')
 })
 
+test(`Simple status as AST`, async t => {
+	const content = await badges('test-1', true)
+	const expected = {
+		type: 'root',
+		children: [
+			{
+				type: 'paragraph',
+				children: [
+					{
+						alt: 'Status',
+						url: 'http://img.shields.io/badge/status-beta-blue.svg?style=flat',
+						type: 'image'
+					},
+					{
+						type: 'text',
+						value: ' '
+					}
+				]
+			},
+			{
+				type: 'break'
+			}
+		]
+	}
+	t.deepEqual(content, expected)
+})
+
