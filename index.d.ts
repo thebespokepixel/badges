e7c5c0b000f6d6fbf70edd0ee0cb3da5e758732c
Mark Griffiths <mark@thebespokepixel.com>
2021-11-02 09:11:08 +0000

    Update Documentation

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,2 @@
 export { render as default };
-/**
- * Render project badge configuration as markdown.
- * @param  {String} context The desired render context i.e: `readme`, `docs` as
- *                          defined in `package.json`.
- * @param  {Boolean} asAST  Render badges as {@link https://github.com/wooorm/mdast|MDAST}
- * @return {Promise}        A promise that resolves to the markdown formatted output.
- */
-declare function render(context: string, asAST?: boolean): Promise<any>;
+declare function render(context: any, asAST?: boolean): Promise<string | import("unist").Parent<import("unist").Node<import("unist").Data>, import("unist").Data>>;
